e03c26757538b31275b534929abff5d4384f8467
Jason Williams <jwilliamstap@gmail.com>
2020-10-25 14:58:09 -0400

    added control buttons and slot titles

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ const users = [
   { id: 4, name: 'curly', slot: 'third' },
   { id: 3, name: 'lucy', slot: 'third', selected: true }
 ];
+
+const lists = document.querySelector('#lists');
+const firstSlot = document.querySelector('#first');
+const secondSlot = document.querySelector('#second');
+const thirdSlot = document.querySelector('#third');
+const selection = document.createElement('button');
+selection.id = 'testborder';
+lists.addEventListener('click', moveListItem);
+
+
+function moveListItem(event) {
+  if(event.target.className.includes('right')) {
+    if(event.target.parentElement.id === 'first') {
+      let user = users[0].name;
+      selection.innerHTML = user;
+      secondSlot.appendChild(selection);
+
+    }
+  }
+}
