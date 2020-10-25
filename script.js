1f1184cf7ad9fffe36e679eaa1eb4014150a5611
Jason Williams <jwilliamstap@gmail.com>
2020-10-25 01:34:34 -0400

    created index, style, and script files. Began assignment.

---
+++ script.js
@@ -0,0 +1,8 @@
+const slots = ['first', 'second', 'third'];
+
+const users = [
+  { id: 1, name: 'moe', slot: 'first' },
+  { id: 2, name: 'larry', slot: 'second' },
+  { id: 4, name: 'curly', slot: 'third' },
+  { id: 3, name: 'lucy', slot: 'third', selected: true }
+];
